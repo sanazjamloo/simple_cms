86627cf37095892bae7c608242e7e3448afcbf21
Sanaz <sanaz.jamloo@gmail.com>
2017-03-14 13:37:14 -0700

    Add erb templates to html files

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,16 +1,16 @@
 class DemoController < ApplicationController
   
   def index
-    #render ('hello')
-    redirect_to('other_hello')
+    render ('hello')
+    #redirect_to(:action => 'other_hello')
   end
   
   def hello
-    redirect_to("http://www.lynda.com")
+  
   end
   
   def other_hello
-    render ('Hello Everyone!')
+    render(:text => 'Hello Everyone!')
   end 
   
 end
