82534f103c1aad60c461c48bbf7ecfb327cf3817
Sanaz <sanaz.jamloo@gmail.com>
2017-03-13 21:35:19 -0700

    Add another method to demo controller

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,11 +1,16 @@
 class DemoController < ApplicationController
   
   def index
-    render ('hello')
+    #render ('hello')
+    redirect_to('other_hello')
   end
   
   def hello
-    render ('Hello Everyone!')
+    redirect_to("http://www.lynda.com")
   end
+  
+  def other_hello
+    render ('Hello Everyone!')
+  end 
   
 end
